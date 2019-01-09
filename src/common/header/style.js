53d7b53bb1a58338075b30a0b01baafcaefa9c3d
rex_yan <rex_yan@126.com>
2019-01-10 00:14:53 +0800

    2019.1.10 v1.0.0, 导航栏

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,93 +1,128 @@
 import styled from 'styled-components';
 
-export const HeaderWrapper = styled.div`
+export const AppHeader = styled.div`
 	position: relative;
-	height: 56px;
-	border-bottom: 1px solid #f0f0f0;
+    z-index: 100;
+    min-width: 1032px;
+    overflow: hidden;
+    background: #FFFFFF;
+    box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+    background-clip: content-box;
+    display: block;
 `;
 
-export const Logo = styled.svg`
-	fill: #0084FF;
-	height: 50px;
-    width: 64px;
-    border: 1px solid red;
+export const HeaderWrapper = styled.div`
+	position: relative;
+	height: 52px;
+	padding: 0 16px;
+    margin: 0 auto;
+    width: 1000px;
+    display: flex;  
+	align-items: center;
+`;
 
-    &.path {
-    	height: 20%;
-    	width: 20%;
-    	border: 1px solid blue;
-    }
+export const Svg = styled.svg`
+	fill: #0084FF;
+	height: 30px;
+    width: 64px;
 `;
 
 export const Nav = styled.div`
-	width: 960px;
-	padding-right: 70px;
-	box-sizing: border-box;
-	height: 100%;
-	margin: 0 auto;
+	display: flex;
+    justify-content: space-between;
+    height: 30px;
+    margin-right: 18px;
+    margin-left: 27px;
+    color: #999999;
 `;
 
 export const NavItem = styled.div`
-	color: #333;
-	font-size: 17px;
-	line-height: 56px;
+	color: #8590A6;
 	padding: 0 15px;
-	&.left {
-		float: left;
-	}
-	&.right {
-		float: right;
-		color: #969696;
-	}
-	&.active {
-		color: #ea6f5a;
-	}
+    font-size: 15px;
+    line-height: 30px;
+    
+    &.first-child{
+    	color: #444444;
+    }
 `;
 
 export const NavSearch = styled.input.attrs({
-	placeholder: '搜索'
+	placeholder: '女大学生因受学校处分自缢'
 })`
-	width: 160px;
-	height: 38px;
-	border: none;
-	outline: none;
-	border-radius: 19px;
-	background: #eee;
-	margin-top: 9px;
-	padding: 0 20px;
-	box-sizing: border-box;
-	font-size; 14px;
-	margin-left: 20px;
-	&::placeholder{
-		color: #999;
-	} 
+	display: flex;
+	position: relative;
+    z-index: 104;
+    width: 326px;
+    position: relative;
+    z-index: 104;
+    height: 34px;
+    padding: 4px 10px;
+    font-size: 14px;
+    border: 1px solid #EBEBEB;
+    border-radius: 3px;
+    box-sizing: border-box;
+    transition: background 200ms, border 200ms;
+    margin-left: 20px;
+    background: #EBEBEB;
 `;
 
 
+export const SearchButton = styled.button`
+	color: #FFFFFF;
+    background-color: #0084FF;
+    z-index: 103;
+    padding: 0 14px;
+    margin-left: 16px;
+    line-height: 30px;
+    text-align: center;
+    cursor: pointer;
+    border: 1px solid;
+    border-radius: 3px;
+    font-size: 14px;
+    height: 35px;
+`
+
 export const Addition = styled.div`
-	position: absolute;
-	right: 0;
-	top: 0;
-	height: 56px;
+	flex: 1;
+    justify-content: flex-end;
+    display: flex;
+    align-items: center;
+`
+
+export const Notifications = styled.div`
+	margin-right: 40px;
+	position: relative;
+    display: inline-block;
 `
 
 export const Button = styled.div`
-	float: right;
-	line-height: 38px;
-	margin-top: 9px;
-	border-radius: 19px;
-	border: 1px solid #ec6149;
-	margin-right: 20px;
-	padding: 0 20px;
-	font-size: 14px;
-	&.reg{
-		color: #ec6149;
-	}
-	&.writting{
-		color: #fff;
-		background: #ec6149;
-	}
+	display: inline-block;
+    padding: 0 16px;
+    font-size: 14px;
+    line-height: 32px;
+    color: #8590A6;
+    text-align: center;
+    cursor: pointer;
+    background: none;
+    border: 1px solid;
+    border-radius: 3px;
+
+    &.pushnotifications-icon{
+    	border:1px solid red;
+	    padding: 0;
+	    line-height: inherit;
+	    background-color: transparent;
+	    border: none;
+	    border-radius: 0
+    }
 `
+export const NotificationSvg = styled.svg`
+    width: 22;
+    height: 22;
+`;
+
+
 export const Searchinfo = styled.div`
 	position: absolute;
 	left: 35%;
