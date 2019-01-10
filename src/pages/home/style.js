ebfd4f75ce42e3f282cf643462c37d2de4c569ee
runsha.yan <runsha.yan@huifu.com>
2019-01-10 17:22:53 +0800

    2019.1.10 v1.0.0, 首页样式

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,129 +1,144 @@
 import styled from 'styled-components';
 
 export const HomeWrapper = styled.div`
+	background: #F6F6F6;
+`;
+
+export const Topstory = styled.div`
+	display: block;
+    font-size: 15px;
+    color: #1A1A1A;
+    -webkit-tap-highlight-color: rgba(26, 26, 26, 0)
+`
+
+export const TopstoryContainer = styled.div`
 	overflow: hidden
-	width: 960px;
-	margin:0 auto;
-`
+	border: 1px solid red;
+	display: flex;
+    align-items: flex-start;
+    width: 1000px;
+    padding: 0 16px;
+    margin: 10px auto
+`;
 
-export const HomeLeft = styled.div`
-	float: left
-	margin-left: 15px;
-	padding-top: 30px;
-	width: 625px;
-	.banner-img {
-		width: 625px;
-		height: 270px;
+export const TopstoryMainColumn = styled.div`
+	flex-shrink: 0;
+    margin-right: 10px;
+    margin-bottom: 0;
+    width: 694px
+`;
+
+export const TopstoryTabCard = styled.div`
+	margin-bottom: 0;
+    box-shadow: none;
+    border-bottom: 1px solid #F0F2F7;
+    width: 100%;
+    background: #FFFFFF;
+    overflow: hidden;
+    border-radius: 2px;
+    box-sizing: border-box;
+`;
+
+export const TabCardUl = styled.ul`
+	display: block;
+    list-style-type: disc;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+    padding: 0;
+    margin: 0;
+    border-bottom: none;
+`;
+
+export const TabCardLi = styled.div`
+	display: inline-block;
+    padding: 0 20px;
+    list-style-type: none;
+    text-align: -webkit-match-parent;
+`;
+
+
+export const DivLabel = styled.div`
+	&.card{
+		margin-bottom: 10px;
+	    background: #FFFFFF;
+	    overflow: hidden;
+	    border-radius: 2px;
+	    box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+	    box-sizing: border-box;
+	};
+
+	&.globalwrite-nav{
+		display: flex;
+    	justify-content: center;
+	};
+
+	&.globalwrite-navtitle{
+		color: #444444;
+    	font-size: 16px;
+    	cursor: pointer;
+	};
+
+	&.globalwrite-draft{
+		align-items: center;
+	    border-top: 1px solid #F0F2F7;
+	    color: #8590A6;
+	    display: flex;
+	    height: 48px;
+	    margin: 0 20px;
+	    font-size: 14px;
+	};
+
+	$.globalsidebar-navtext{
+		flex: 1;
+    	margin: 0 10px;
+    	text-align: left;
+	};
+
+	&.globalsidebar-navnumber{
+		padding: 6px 10px;
+	    border-radius: 2px;
+	    background: #F6F6F6;
+	    color: #8590A6;
+	    font-size: 12px;
+	};
+`;
+
+
+export const ALabel = styled.a`
+	&.globalwrite-navitem{
+		align-items: center;
+	    cursor: pointer;
+	    display: flex;
+	    flex-direction: column;
+	    margin: 23px;
 	}
-`
 
-export const HomeRight = styled.div`
-	width: 280px;
-	float: right;
-`
-
-
-export const TopicWrapper = styled.div`
-	overflow: hidden
-	padding: 20px 0 10px 0;
-	margin-left: -10px;
-	border-bottom: 1px solid #dcdcdc;
-`
-
-export const TopicItem = styled.div`
-	float: left;
-	background: #f7f7f7
-	height: 32px;
-	padding-rigin; 10px;
-	margin-left: 18px;
-	line-height: 32px;
-	font-size: 14px;
-	color: #000;
-	border: 1px solid #dcdcdc;
-	border-radius: 4px;
-	margin-bottom: 18px;
-	.topic-pic{
-		width: 32px;
-		height: 32px;
-		display: block;
-		float: left;
-		margin-right: 10px;
+	&.globalwidebar-navlink{
+		display: flex;
+	    align-items: center;
+	    padding: 0 20px;
+	    height: 40px;
+	    overflow: hidden;
 	}
-`
+`;
 
-export const ListItem = styled.div`
-	overflow: hidden;
-	padding: 20px 0;
-	border-bottom: 1px solid #dcdcdc;
-	.pic {
-		display: block;
-		width: 125px;
-		height: 100px;
-		float: right;
-		border-radius: 10px;
+export const SvgLabel = styled.svg`
+	&.globalwrite-navicon{
+    	margin-bottom: 6px;
+    	width: 24px;
+    	height: 24px;
 	}
-	
-`
 
-export const ListInfo = styled.div`
-	width: 500px;
-	float: left;
-	.title {
-		line-height: 27px;
-		font-size: 18px;
-		font-weight: bold;
+	&.zi-draft{
+		fill: currentcolor;
+    	width: 18px;
+    	height: 18px;
 	}
-	.desc {
-		line-height: 24px;
-		font-size: 13px;
-		color: #999;
-	}
-`
 
-export const RecommendWrapper = styled.div`
-	margin: 30px 0;
-	width: 280px;
-`
-	
-export const RecommendItem = styled.div`
-	width: 280px;
-	height: 50px;
-	background :url(${(props) => props.imgUrl})
-`
-
-export const WriterWrapper = styled.div`
-	width: 278px;
-	border: 1px solid #dcdcdc;
-	border-radius: 3px;
-	height: 300px;
-	line-height: 300px;
-	text-alien:center;
-`
-
-export const LoadMore = styled.div`
-	width: 100%;
-	height: 40px;
-	line-height: 40px;
-	margin: 30px 0;
-	text-align: center;
-	background: #a5a5a5
-	border-radius: 20px;
-	color: #fff;
-	cursor: pointer;
-`
-
-export const BackTop = styled.div`
-	position: fixed;
-	right: 100px;
-	bottom: 100px;
-	width: 60px;
-	height: 60px;
-	line-height: 60px;
-	text-align: center;
-	border: 1px solid #ccc;
-	font-size: 14px;
-`
-
+`;	
 
 
 
